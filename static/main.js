5884185e079e8b8d1117b3373703518a71924bc2
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-03-25 17:00:55 +0100

    Small refactor. Added favico

--- static/main.js
+++ static/main.js
@@ -1,51 +1,38 @@
 const URL = "http://127.0.0.1:8000";
-const temperature = document.getElementById("temperature");
-const day = document.getElementById("day");
-const time = document.getElementById("time");
-const lightDIV = document.getElementById("lights");
+const TEMPERATURE_ELEMENT = document.getElementById("temperature");
+const DAY_ELEMENT = document.getElementById("day");
+const TIME_ELEMENT = document.getElementById("time");
+const LIGHT_ELEMENT = document.getElementById("lights");
 
-const lightIconON = `<i class="bi bi-lightbulb-fill"></i>`;
-const lightIconOFF = `<i class="bi bi-lightbulb"></i>`;
+const LIGHT_ICON_ON = `<i class="bi bi-lightbulb-fill"></i>`;
+const LIGHT_ICON_OFF = `<i class="bi bi-lightbulb"></i>`;
 
-// let lightbulb = {
-//     name: "Light 1",
-//     status: false,
-// };
-
-let lights = {};
-
-// Set request headers
 const headers = {
     "Content-Type": "application/json",
 };
 
+let lights = {};
+
 function getTemp() {
     fetch(URL + "/temperature")
         .then((response) => response.json())
-        .then((json) => (temperature.innerHTML = json["temp"]))
+        .then((json) => (TEMPERATURE_ELEMENT.innerHTML = json["temp"]))
         .catch((err) => console.log(err));
 }
 
 function getDay() {
     fetch(URL + "/day")
         .then((response) => response.json())
-        .then((json) => (day.innerHTML = json["day"]))
+        .then((json) => (DAY_ELEMENT.innerHTML = json["day"]))
         .catch((err) => console.log(err));
 }
 
 function getTime() {
     fetch(URL + "/time")
         .then((response) => response.json())
-        .then((json) => (time.innerHTML = json["time"]))
+        .then((json) => (TIME_ELEMENT.innerHTML = json["time"]))
         .catch((err) => console.log(err));
 }
-
-// function getLightbulb() {
-//     fetch(URL + "/lightbulb")
-//         .then((response) => response.json())
-//         .then((json) => updateLightbulbDIV(json))
-//         .catch((err) => console.log(err));
-// }
 
 function getLights() {
     fetch(URL + "/lightbulb")
@@ -55,10 +42,10 @@ function getLights() {
             let res = "";
             for ([key, value] of Object.entries(lights)) {
                 let icon;
-                if (value["status"]) {
-                    icon = lightIconON;
+                if (value.status) {
+                    icon = LIGHT_ICON_ON;
                 } else {
-                    icon = lightIconOFF;
+                    icon = LIGHT_ICON_OFF;
                 }
                 res += `
                 <div class="row pb-3">
@@ -70,7 +57,7 @@ function getLights() {
                     </div>
                 </div>`;
             }
-            lightDIV.innerHTML = res;
+            LIGHT_ELEMENT.innerHTML = res;
         })
         .catch((err) => console.log(err));
 }
@@ -87,9 +74,9 @@ function switchLight(name) {
             lights[name] = json;
             let element = document.getElementById(name + "-icon");
             if (lights[name].status) {
-                element.innerHTML = lightIconON;
+                element.innerHTML = LIGHT_ICON_ON;
             } else {
-                element.innerHTML = lightIconOFF;
+                element.innerHTML = LIGHT_ICON_OFF;
             }
         })
         .catch((err) => console.error(err));
